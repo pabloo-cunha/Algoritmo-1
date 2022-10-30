b5ef07e7a66d1f1339442d28a8292b623d010aca
Nicole Souza <108769169+NicSouza@users.noreply.github.com>
2022-10-30 18:25:54 -0300

    Update questao2.py

--- questao2.py
+++ questao2.py
@@ -1,15 +1,15 @@
-salarioFixo = int(input('Salario do funcionario: '))
-vendas = int(input('Valor da vendas: '))
+salarioFixo = int(input('Salário do funcionário: '))
+vendas = int(input('Valor das vendas: '))
 
 if vendas <= 1500:
     bonus5 = vendas * 0.05
     salarioTotal = bonus5 + salarioFixo
 
-    print(f'O salario total será de {salarioTotal} Reais')
+    print(f'O salário total será de {salarioTotal} reais')
 
 if vendas > 1500:
     bonus5 = 1500 * 0.05
     bonus7 = (vendas - 1500) * 0.07
     salarioTotal = bonus5 + bonus7 + salarioFixo
     
-    print(f'O salario total será de {salarioTotal} Reais')
+    print(f'O salário total será de {salarioTotal} reais')
